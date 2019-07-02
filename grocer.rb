9bdf659910277db3af07c08fa1271bec5324eda6
Laura Radville <laura.radville@gmail.com>
2019-07-02 12:07:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,6 +34,28 @@ def apply_coupons(cart, coupons)
   cart
 end
 
+items) do
+    [
+      {"AVOCADO" => {:price => 3.00, :clearance => true}},
+      {"KALE" => {:price => 3.00, :clearance => false}},
+      {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+      {"ALMONDS" => {:price => 9.00, :clearance => false}},
+      {"TEMPEH" => {:price => 3.00, :clearance => true}},
+      {"CHEESE" => {:price => 6.50, :clearance => false}},
+      {"BEER" => {:price => 13.00, :clearance => false}},
+      {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+      {"BEETS" => {:price => 2.50, :clearance => false}},
+      {"SOY MILK" => {:price => 4.50, :clearance => true}}
+    ]
+  end
+
+  let(:coupons) do
+    [
+      {:item => "AVOCADO", :num => 2, :cost => 5.00},
+      {:item => "BEER", :num => 2, :cost => 20.00},
+      {:item => "CHEESE", :num => 3, :cost => 15.00}
+    ]
+
 def apply_clearance(cart)
   cart.each do |element|
     item_name = element[0]
