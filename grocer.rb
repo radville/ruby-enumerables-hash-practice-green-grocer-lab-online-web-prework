d51a7ed05148e92f31880d8841a0bbcc47cf2f5d
Laura Radville <laura.radville@gmail.com>
2019-07-02 12:20:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,16 +34,7 @@ def apply_coupons(cart, coupons)
   cart
 end
 
-items = {{"AVOCADO" => {:price => 3.00, :clearance => true}},
-      {"KALE" => {:price => 3.00, :clearance => false}},
-      {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
-      {"ALMONDS" => {:price => 9.00, :clearance => false}},
-      {"TEMPEH" => {:price => 3.00, :clearance => true}},
-      {"CHEESE" => {:price => 6.50, :clearance => false}},
-      {"BEER" => {:price => 13.00, :clearance => false}},
-      {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
-      {"BEETS" => {:price => 2.50, :clearance => false}},
-      {"SOY MILK" => {:price => 4.50, :clearance => true}}}
+items = {{"AVOCADO" => {:price => 3.00, :clearance => true}},{"KALE" => {:price => 3.00, :clearance => false}}, {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},{"ALMONDS" => {:price => 9.00, :clearance => false}},{"TEMPEH" => {:price => 3.00, :clearance => true}},{"CHEESE" => {:price => 6.50, :clearance => false}},{"BEER" => {:price => 13.00, :clearance => false}}}
 
 coupons =
     [
