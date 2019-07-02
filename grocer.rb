c1822404188536fe878ab06c3b90ebd10299dd74
Laura Radville <laura.radville@gmail.com>
2019-07-02 14:24:49 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,6 +21,8 @@ end
 
 # array = ["BEER" => {:price => 13.00, :clearance => false, :count => 3}, "BEETS" => {:price => 2.50, :clearance => false, :count => 1}]
 
+array = ["CHEESE" => {:price => 6.50, :clearance => false, :count => 5}]
+
 # # # consolidate_cart(array)
 
 # hash = {"BEER" => {:price => 13.00, :clearance => false, :count => 3},
